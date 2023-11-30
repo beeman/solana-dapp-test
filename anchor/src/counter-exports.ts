087bb0ada182b098d585ab5fe66edf202204825a
Bram Borggreve <beeman@beeman.nl>
2023-11-30 22:27:56 +0000

    chore: initial commit

---
+++ anchor/src/counter-exports.ts
@@ -0,0 +1,19 @@
+import { Program } from '@coral-xyz/anchor'
+import { Cluster, PublicKey } from '@solana/web3.js'
+import { IDL as CounterIDL } from '../target/types/counter'
+import type { Counter } from '../target/types/counter'
+
+export { Counter, CounterIDL }
+export type CounterProgram = Program<Counter>
+export const COUNTER_PROGRAM_ID = new PublicKey('EcUiXRRRJcW8FVaNu6X7pfd6AmhtMJ2X6EaqrEUeTKq9')
+
+export function getCounterProgramId(cluster: Cluster) {
+  switch (cluster) {
+    case 'devnet':
+    case 'testnet':
+    case 'mainnet-beta':
+      return new PublicKey('CounNZdmsQmWh7uVngV9FXW2dZ6zAgbJyYsvBpqbykg')
+    default:
+      return COUNTER_PROGRAM_ID
+  }
+}
